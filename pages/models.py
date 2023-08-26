5ab4471ad156bc1ae7ecf599abd708d69e7bc039
lex <juszczaa@office.wit.edu.pl>
2023-08-26 04:34:27 +0200

    Creats Letter model for submissions

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Letter(models.Model):
+    displayed_name = models.CharField(max_length=120)
+    message = models.TextField(max_length=1000)
+    reviewed = models.BooleanField(default=False)
+    publish = models.BooleanField(default=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return f'{self.displayed_name} | {self.message[:50]}'
+
+
