670c53e05bc38eeabf2c01602cdba78b141a57f9
lex <juszczaa@office.wit.edu.pl>
2023-08-26 04:32:48 +0200

    Refactors views.py

--- pages/views.py
+++ pages/views.py
@@ -5,17 +5,22 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def contact(request):
     return render(request, 'contact.html')
 
+
 def wall(request):
     return render(request, 'wall.html')
 
+
 def submissions(request):
     return render(request, 'submissions.html')
+
 
 def resources(request):
     return render(request, 'home.html')
